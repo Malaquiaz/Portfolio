c886d47ddf4884123179a84c5d3619cf80fccaec
Higor Vieira <74118081+Malaquiaz@users.noreply.github.com>
2025-05-09 23:50:16 -0300

    avatar

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -1,8 +1,14 @@
+import Foto from '../assets/fotografia.jpg';
+
 function Sobre() {
   return (
     <div className="sobre">
       <h1>Sobre mim</h1>
       <div className="sobre-info">
+        <img 
+                src={Foto}
+                className="sobre-imagem"
+        />
         <p><strong> Apresentação: </strong> Olá, eu me chamo Higor Vieira da Franca. Sou estudante de Desenvolvimento de Software Multiplataforma na Fatec Itaquera (2022 - 2025).</p>
         <p><strong>Curso de Extensão</strong> Inteligencia Artificial e Machine Learning com Reconhecimento Facial</p>
         <p><strong>Proficiência em Inglês:</strong> Inglês de nível básico</p>
@@ -14,3 +20,4 @@ function Sobre() {
 }
 
 export default Sobre;
+
