ee3f3feae62e2f5524717379aa55f300bd73f335
Higor Vieira <74118081+Malaquiaz@users.noreply.github.com>
2025-05-10 15:07:06 -0300

    miniaturas?

--- src/components/ListaProjetos.jsx
+++ src/components/ListaProjetos.jsx
@@ -2,14 +2,18 @@ import { Link, useParams } from "react-router-dom";
 import { FaArrowRight } from "react-icons/fa";
 import projetos from "../assets/projetos.json";
 import "../styles/ListaProjetos.css"; 
+import miniaturas from "./miniaturas";
 
 function ListaProjetos() {
-
   return (
     <div className="projetos-lista">
       {projetos.map((projeto) => (
         <div key={projeto.id} className="projeto-item">
-          <img src={projeto.imagem} alt={`Imagem de ${projeto.nome}`} className="projeto-imagem" />
+          <img
+            src={miniaturas[projeto.imagem]} // usa o campo imagem do JSON para pegar
+            alt={`Miniatura de ${projeto.nome}`}
+            className="projeto-imagem"
+          />
 
           <div className="projeto-info">
             <span className="projeto-titulo">{projeto.nome}</span>
@@ -17,7 +21,6 @@ function ListaProjetos() {
               <span className="projeto-descricao">{projeto.descricao}</span>
             )}
           </div>
-
           <Link
             to={`/projeto/${projeto.id}`} // redireciona para a página do projeto
             className="projeto-botao"
---
+++ src/components/miniaturas.jsx
@@ -0,0 +1,10 @@
+import github from "../assets/lista_miniaturas/github.png";
+import expo from "../assets/lista_miniaturas/expo.png";
+
+
+const miniaturas = {
+  "github.png": github,
+  "expo.png": expo,
+};
+
+export default miniaturas;
