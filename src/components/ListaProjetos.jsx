d55220e3d721de4d374224d3b7a33db34d730842
Higor Vieira <74118081+Malaquiaz@users.noreply.github.com>
2025-05-09 21:05:52 -0300

    a

--- src/components/ListaProjetos.jsx
+++ src/components/ListaProjetos.jsx
@@ -1,40 +1,47 @@
 import React from "react";
 import { FaArrowRight } from "react-icons/fa";
-import "../styles/ListaProjetos.css";
+import "../styles/ListaProjetos.css"; // já está certo
+
+import cinza1 from '../assets/project_icons/cinza1.png';
+import azul1 from '../assets/project_icons/azul1.png';
+import amarelo2 from '../assets/project_icons/amarelo2.png';
 
 function ListaProjetos() {
   const projetos = [
-    { id: 1, nome: "aishjdiasd", link: "https://github.com/seuusuario/projeto1" },
-     { id: 1, nome: "aishjdiasd", link: "https://github.com/seuusuario/projeto1" },
-      { id: 1, nome: "aishjdiasd", link: "https://github.com/seuusuario/projeto1" },
-       { id: 1, nome: "aishjdiasd", link: "https://github.com/seuusuario/projeto1" },
-        { id: 1, nome: "aishjdiasd", link: "https://github.com/seuusuario/projeto1" },
-         { id: 1, nome: "aishjdiasd", link: "https://github.com/seuusuario/projeto1" },
-          { id: 1, nome: "aishjdiasd", link: "https://github.com/seuusuario/projeto1" },
-           { id: 1, nome: "aishjdiasd", link: "https://github.com/seuusuario/projeto1" },
-            { id: 1, nome: "aishjdiasd", link: "https://github.com/seuusuario/projeto1" },
-
-
+    { id: 1, nome: "Projeto", link: "https://github.com/Malaquiaz/projeto1", imagem: cinza1, descricao: "Testezinho" },
+    { id: 2, nome: "aishjdiasd", link: "https://github.com/Malaquiaz/projeto2", imagem: azul1, descricao: "" },
+    { id: 3, nome: "aishjdiasd", link: "https://github.com/Malaquiaz/projeto3", imagem: amarelo2, descricao: "" },
+    // continue...
   ];
 
   return (
-    <div className="projetos-lista">
-      {projetos.map((projeto) => (
-        <div key={projeto.id} className="projeto-item">
-          <span className="projeto-texto">
-            {projeto.id} - {projeto.nome}
+  <div className="projetos-lista">
+    {projetos.map((projeto) => (
+      <div key={projeto.id} className="projeto-item">
+        <img src={projeto.imagem} alt={`Imagem de ${projeto.nome}`} className="projeto-imagem" />
+        
+        <div className="projeto-info"> {/* NOVA DIV agrupando */}
+          <span className="projeto-titulo">
+            {projeto.nome}
           </span>
-          <a
-            href={projeto.link}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="projeto-botao"
-          >
-            <FaArrowRight className="projeto-icone" />
-          </a>
+          {projeto.descricao && ( // descrição só se existir
+            <span className="projeto-descricao">
+              {projeto.descricao}
+            </span>
+          )}
         </div>
-      ))}
-    </div>
+
+        <a
+          href={projeto.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="projeto-botao"
+        >
+          <FaArrowRight className="projeto-icone" />
+        </a>
+      </div>
+    ))}
+  </div>
   );
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,6 +8,7 @@ function NavBar() {
         <li><a href="#home">Home</a></li>
         <li><a href="#projetos">Projetos</a></li>
         <li><a href="#sobre">Sobre</a></li>
+        <li><a href="#contato">Contato</a></li>
       </ul>
     </nav>
   );
